f076890d4e164f4e6d6a02a5261fa62dd9d525d7
FatumaLiban2 <fatuma.omar@strathmore.edu>
2025-06-03 14:16:57 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+
+    public class main {
+     public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        Student student = new Student();
+        Coach coach = new Coach();
+
+        teacher.teach();
+        teacher.markAttendance();
+        student.attendClass();
+        coach.playSport();
+    }
+}
+
+
---
+++ schoolmember.java
@@ -0,0 +1,38 @@
+//interfaces
+interface TeacherDuties {
+    void teach();
+    void markAttendance();
+}
+
+interface StudentDuties {
+    void attendClass();
+}
+
+interface SportsDuties {
+    void playSport();
+}
+
+// Implementing only what is needed
+class Teacher implements TeacherDuties {
+    public void teach() {
+        System.out.println("Teaching subjects.");
+    }
+
+    public void markAttendance() {
+        System.out.println("Marking student attendance.");
+    }
+}
+
+class Student implements StudentDuties {
+    public void attendClass() {
+        System.out.println("Attending class.");
+    }
+}
+
+class Coach implements SportsDuties {
+    public void playSport() {
+        System.out.println("Coaching sports.");
+    }
+}
+
+
